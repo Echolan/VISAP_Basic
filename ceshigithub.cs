09fb1f260aae870456f82b5174a1de550f5d07ea
Cg87521 <chenggang87521@163.com>
2016-01-14 12:08:25 +0800

    add

---
+++ ceshigithub.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ceshigit
+{
+    class ceshigithub
+    {
+        static void Main(string[] args)
+        { for (; ; )
+            {
+                Console.WriteLine("Enter a number:");
+                double num_sqrt = Convert.ToDouble(Console.ReadLine());
+                double num_ori = 9;
+                for (int i = 1; i <= 100; i++)
+                {
+                    num_ori = (num_ori + num_sqrt / num_ori) / 2;
+                }
+                Console.WriteLine("{0}", num_ori);
+        }
+    }
+}
